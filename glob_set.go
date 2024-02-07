5a966a3962c230572792f7f7412bf228f7d9d414
superp00t <superp00t@tutanota.com>
2024-02-07 01:44:05 -0500

    fix(mpq): avoid duplicate MPQs being loaded into Set

--- glob_set.go
+++ glob_set.go
@@ -9,6 +9,7 @@ func GlobSet(patterns ...string) (set *Set, err error) {
 	set = NewSet()
 
 	// glob archive paths
+	var matched = map[string]bool{}
 	var archive_list []string
 	var matched_by_pattern []string
 	var archive *Archive
@@ -23,11 +24,15 @@ func GlobSet(patterns ...string) (set *Set, err error) {
 
 	// attempt to open globbed archive paths
 	for _, archive_path := range archive_list {
-		archive, err = Open(archive_path)
-		if err != nil {
-			return
+		if !matched[archive_path] {
+			archive, err = Open(archive_path)
+			if err != nil {
+				return
+			}
+
+			set.archives = append(set.archives, archive)
+			matched[archive_path] = true
 		}
-		set.archives = append(set.archives, archive)
 	}
 
 	return
