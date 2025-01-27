651347da2df6573e34ac84d05651c998eaf626fc
superp00t <superp00t@tutanota.com>
2025-01-27 01:59:28 -0500

    refactor(info): removed old hash types

--- info/hash_constants.go
+++ info/hash_constants.go
@@ -1,13 +1,5 @@
 package info
 
-// const (
-// 	HashTableIndex = 0x000
-// 	HashNameA      = 0x100
-// 	HashNameB      = 0x200
-// 	HashFileKey    = 0x300
-// 	HashKey2Mix    = 0x400
-// )
-
 // Special block indices
 const (
 	HashTerminator uint32 = 0xFFFFFFFF
